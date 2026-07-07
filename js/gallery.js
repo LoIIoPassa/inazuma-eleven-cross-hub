3970e1934277fb4410f137900602a75d9acb751c
LoIIoPassa <kfbmcg7ggd@privaterelay.appleid.com>
2026-07-07 10:11:22 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -6,14 +6,12 @@ function renderGallery() {
 
     grid.innerHTML = characterRegistry.map(char => {
         const starsHTML = Array(char.stars).fill(
-            `<img src="img/Frm_GachaIcon/Icon_GradeStar.png" class="star-icon">`
+            `<img src="/img/Frm_GachaIcon/Icon_GradeStar.png" class="star-icon">`
         ).join('');
 
         return `
         <div class="col-6 col-md-3 col-lg-2 mb-4 d-flex flex-column align-items-center">
-            <!-- TORNATO ALLA VERSIONE STABILE: punta al file vero .html e salva in memoria per sicurezza -->
-            <a href="character.html?id=${char.id}" onclick="localStorage.setItem('selectedChar', '${char.id}')" style="text-decoration: none; color: inherit;">                
-                
+            <a href="/characters/${char.id}" onclick="localStorage.setItem('selectedChar', '${char.id}')" style="text-decoration: none; color: inherit;">                
                 <div class="gacha-icon-wrapper">
                     <div class="rarity-bg-layer" style="background-image: url('${char.background}');">
                         <img src="${char.thumb}" class="personaje-imagen" alt="char">
@@ -23,15 +21,11 @@ function renderGallery() {
                         <img src="${char.element}" class="atributo-tag">
                         <img src="${char.position}" class="posicion-tag">
                     </div>
-                    <div class="stars-curved-container">
-                        ${starsHTML}
-                    </div>
+                    <div class="stars-curved-container">${starsHTML}</div>
                 </div>
-
                 <div class="personaje-nombre d-flex flex-column justify-content-center">
                     <span>${char.name}</span>
                 </div>
-
             </a>
         </div>`;
     }).join('');
